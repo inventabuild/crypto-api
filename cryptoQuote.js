59195122034d9a42da4016d9e9607f4c9ebcb461
inventabuild <mrabushka@hotmail.com>
2022-03-26 23:21:31 -0500

    Overwrote js, html, css files in VS with newer codesandbox files that have working input field,  buttons and API that get quotes

--- cryptoQuote.js
+++ cryptoQuote.js
@@ -1,59 +1,90 @@
-const inputField = document.querySelector("[name=name]");
-const form = document.querySelector(".js-search-form");
-// form.addEventListener("submit", cryptoQuote);
-const listAllButton = document.querySelector(".js-listAll");
-const cryptoQuoteButton = document.querySelector(".js-cryptoQuote");
+const inputField = document.querySelector("[name=userInput]");
+const form = document.querySelector(".js-form");
+const cryptoInputField = document.querySelector(".js-cryptoInputField");
+const listAllBtn = document.querySelector(".js-listAll");
+const searchBtn = document.querySelector(".js-search");
+var cryptoInputFromUser = "";
+var listAllClicked = "False";
 
-function cryptoQuote (event) {
-    // event.preventDefault();
-    debugger;
-    const searchExpression = inputField.value;
-    inputField.value = "";
-    let API_URL = `https://api.coingecko.com/api/v3/coins/`;
+form.addEventListener("submit", cryptoQuote);
+// listAllBtn.addEventListener("click", listAllFunction);
+// searchBtn.addEventListener("click", searchFunction);
+function listAllFunction(event) {
+  event.preventDefault();
+  debugger;
+  alert(event.target.id);
+  if (event.target.id === "js-listAll") {
+    alert("List All Triggered");
+  }
+
+  listAllClicked = "True";
+  cryptoInputFromUser = cryptoInputField.value;
+  cryptoQuote();
+}
+function searchFunction(event) {
+  event.preventDefault();
+  debugger;
+  alert(event);
+  listAllClicked = "False";
+  cryptoInputFromUser = cryptoInputField.value;
+  cryptoQuote(event);
+}
+function cryptoQuote(event) {
+  event.preventDefault();
+  cryptoInputFromUser = cryptoInputField.value;
+  const searchExpression = inputField.value;
+  inputField.value = "";
+  let API_URL = `https://api.coingecko.com/api/v3/coins/`;
+  if (event.submitter.id === "js-search") {
+    listAllClicked = "False";
     if (searchExpression.length > 0) {
-        API_URL += `${searchExpression}`;
+      API_URL += `${searchExpression}`;
     }
-    fetch(API_URL)
-        .then((data) => data.json())
-        .then(renderCrypto);
+  } else {
+    listAllClicked = "True";
+  }
+  fetch(API_URL)
+    .then((data) => data.json())
+    .then(buildQuoteTable);
+}
+function buildQuoteTable(response) {
+  if (!Array.isArray(response)) {
+    response = [response];
+  }
+  var header = "";
+  var row = "";
+  header = `<tr class="quote-table">
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Current Price (USD)</th>
+          </tr>`;
+  document.querySelector(".js-quote-table-body").innerHTML = header;
+  if (listAllClicked === "True") {
+    for (let i = 0; i < response.length; i++) {
+      row = `<tr>
+              <td>${response[i].name}</td>
+              <td>${response[i].symbol}</td>
+              <td>${response[i].market_data.current_price.usd}</td>
+            </tr>`;
+      document.querySelector(".js-quote-table-body").innerHTML += row;
     }
-
-    function renderCrypto(response) {
-        // alert(response.id)
-        // alert(response.market_data.current_price.usd)
-        // const cryptQuote = response.data;
-        let htmlTemplate = "";
-        for (let i = 0; i < response.length; i++) {
-        htmlTemplate += `<li>${response[i].id}</li>`
-        }
-        // <div class="quote">
-        //     <h4>${response.id}</h4>
-        // </div>
-        document.querySelector(".js-crypto-container").innerHTML = htmlTemplate;
-        debugger;
-        if (listAllClicked == "True") {
-            debugger;
-            // listAllConst.addEventListener("click", listAllFunction);
-        }
-        else if (document.getElementById(".js-cryptoQuote").click == "True") {
-            // cryptoQuoteConst.addEventListener("click", cryptoQuoteFunction);
-        }
+  } else {
+    // alert(response.name, cryptoInputFromUser);
+    var success = "False";
+    for (let i = 0; i < response.length; i++) {
+      if (response[i].id === cryptoInputFromUser) {
+        row = `<tr>
+              <td>${response[i].name}</td>
+              <td>${response[i].symbol}</td>
+              <td>${response[i].market_data.current_price.usd}</td>
+            </tr>`;
+        document.querySelector(".js-quote-table-body").innerHTML += row;
+        i = response.length;
+        success = "True";
+      }
     }
-
-    var listAllClicked = "False"
-    // listAllButton.addEventListener("click", alert("test"));
-
-    function listAllFunction(event) {
-        listAllClicked == "True"
-        cryptoQuote
+    if (success === "False") {
+      alert("Your crypto selection is not in our database");
     }
-
-    function PopPop () {
-        alert("Pop");
-    }
-
-
-    function cryptoQuoteFunction(event) {
-        debugger;
-        alert("cryptoQuote");
-    }
+  }
+}
